503ba4f2c4839cb92467a389bac13139aca54989
Justin Kenney <justinkenney13@gmail.com>
2018-11-08 15:38:16 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+  "Hello, World!"
+  end
+
+  # get '/name' do
+  #   "My name is Justin"
+  # end
+
+  get('/name'){"My name is Justin"}
+
+
+  # get '/hometown' do
+  #   "My hometown is Anamop"
+  # end
+
+  get('/hometown'){"My hometown is Anamop"}
+
+  get('/favorite-song'){"My favorite song is Orion"}
 end
