27725f381bc78b6d465e3b68efc704d6fcf0cc13
Justin Kenney <justinkenney13@gmail.com>
2018-11-08 15:35:41 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+  "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Justin"
+  end
+
+  get '/hometown' do
+    "My hometown is Anamop"
+  end
+
+  get('favorite-song'){"My favorite song is Orion"}
 end
